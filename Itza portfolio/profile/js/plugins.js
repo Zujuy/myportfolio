bac7509a98c7a37edd95f80680ae54d0a0d20df7
Itza Zujuy Blanco <39780663+Zujuy@users.noreply.github.com>
2020-01-24 19:31:25 -0600

    starting with gallery

--- Itza portfolio/profile/js/plugins.js
+++ Itza portfolio/profile/js/plugins.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+(function () {
     var method;
-    var noop = function () {};
+    var noop = function () { };
     var methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
@@ -21,36 +21,25 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
-
-
-/**
- * Single Page Nav Plugin
- * Copyright (c) 2013 Chris Wojcik <hello@chriswojcik.net>
- * Dual licensed under MIT and GPL.
- * @author Chris Wojcik
- * @version 1.1.0
- */
-
 // Utility
 if (typeof Object.create !== 'function') {
-    Object.create = function(obj) {
-        function F() {}
+    Object.create = function (obj) {
+        function F() { }
         F.prototype = obj;
         return new F();
     };
 }
 
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
     "use strict";
-    
+
     var SinglePageNav = {
-        
-        init: function(options, container) {
-            
+
+        init: function (options, container) {
+
             this.options = $.extend({}, $.fn.singlePageNav.defaults, options);
-            
-            this.container = container;            
+
+            this.container = container;
             this.$container = $(container);
             this.$links = this.$container.find('a');
 
@@ -60,7 +49,7 @@ if (typeof Object.create !== 'function') {
 
             this.$window = $(window);
             this.$htmlbody = $('html, body');
-            
+
             this.$links.on('click.singlePageNav', $.proxy(this.handleClick, this));
 
             this.didScroll = false;
@@ -68,16 +57,16 @@ if (typeof Object.create !== 'function') {
             this.setTimer();
         },
 
-        handleClick: function(e) {
-            var self  = this,
-                link  = e.currentTarget,
-                $elem = $(link.hash);  
+        handleClick: function (e) {
+            var self = this,
+                link = e.currentTarget,
+                $elem = $(link.hash);
 
-            e.preventDefault();             
+            e.preventDefault();
 
             if ($elem.length) { // Make sure the target elem exists
 
-                
+
                 // Prevent active link from cycling during the scroll
                 self.clearTimer();
 
@@ -87,9 +76,9 @@ if (typeof Object.create !== 'function') {
                 }
 
                 self.setActiveLink(link.hash);
-                
-                self.scrollTo($elem, function() { 
-                 
+
+                self.scrollTo($elem, function () {
+
                     if (self.options.updateHash) {
                         document.location.hash = link.hash;
                     }
@@ -100,21 +89,21 @@ if (typeof Object.create !== 'function') {
                     if (typeof self.options.onComplete === 'function') {
                         self.options.onComplete();
                     }
-                });                            
-            }     
+                });
+            }
         },
-        
-        scrollTo: function($elem, callback) {
+
+        scrollTo: function ($elem, callback) {
             var self = this;
             var target = self.getCoords($elem).top;
             var called = false;
 
             self.$htmlbody.stop().animate(
-                {scrollTop: target}, 
-                { 
+                { scrollTop: target },
+                {
                     duration: self.options.speed,
-                    easing: self.options.easing, 
-                    complete: function() {
+                    easing: self.options.easing,
+                    complete: function () {
                         if (typeof callback === 'function' && !called) {
                             callback();
                         }
@@ -123,77 +112,77 @@ if (typeof Object.create !== 'function') {
                 }
             );
         },
-        
-        setTimer: function() {
+
+        setTimer: function () {
             var self = this;
-            
-            self.$window.on('scroll.singlePageNav', function() {
+
+            self.$window.on('scroll.singlePageNav', function () {
                 self.didScroll = true;
             });
-            
-            self.timer = setInterval(function() {
+
+            self.timer = setInterval(function () {
                 if (self.didScroll) {
                     self.didScroll = false;
                     self.checkPosition();
                 }
             }, 250);
-        },        
-        
-        clearTimer: function() {
+        },
+
+        clearTimer: function () {
             clearInterval(this.timer);
             this.$window.off('scroll.singlePageNav');
             this.didScroll = false;
         },
-        
+
         // Check the scroll position and set the active section
-        checkPosition: function() {
+        checkPosition: function () {
             var scrollPos = this.$window.scrollTop();
             var currentSection = this.getCurrentSection(scrollPos);
             this.setActiveLink(currentSection);
-        },        
-        
-        getCoords: function($elem) {
+        },
+
+        getCoords: function ($elem) {
             return {
                 top: Math.round($elem.offset().top) - this.options.offset
             };
         },
-        
-        setActiveLink: function(href) {
+
+        setActiveLink: function (href) {
             var $activeLink = this.$container.find("a[href='" + href + "']");
-                            
+
             if (!$activeLink.hasClass(this.options.currentClass)) {
                 this.$links.removeClass(this.options.currentClass);
                 $activeLink.addClass(this.options.currentClass);
             }
-        },        
-        
-        getCurrentSection: function(scrollPos) {
+        },
+
+        getCurrentSection: function (scrollPos) {
             var i, hash, coords, section;
-            
+
             for (i = 0; i < this.$links.length; i++) {
                 hash = this.$links[i].hash;
-                
+
                 if ($(hash).length) {
                     coords = this.getCoords($(hash));
-                    
+
                     if (scrollPos >= coords.top - this.options.threshold) {
                         section = hash;
                     }
                 }
             }
-            
+
             // The current section or the first link
             return section || this.$links[0].hash;
         }
     };
-    
-    $.fn.singlePageNav = function(options) {
-        return this.each(function() {
+
+    $.fn.singlePageNav = function (options) {
+        return this.each(function () {
             var singlePageNav = Object.create(SinglePageNav);
             singlePageNav.init(options, this);
         });
     };
-    
+
     $.fn.singlePageNav.defaults = {
         offset: 0,
         threshold: 120,
@@ -205,7 +194,7 @@ if (typeof Object.create !== 'function') {
         onComplete: false,
         beforeStart: false
     };
-    
+
 })(jQuery, window, document);
 
 
